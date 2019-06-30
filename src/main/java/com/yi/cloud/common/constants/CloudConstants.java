f41eb2266b1335bd9b01db24823f3c68b8603561
DESKTOP-9OHMB5J\Chen <1332787671@qq.com>
2019-06-30 21:19:32 +0800

    提交工具类代码

---
+++ src/main/java/com/yi/cloud/common/constants/CloudConstants.java
@@ -0,0 +1,11 @@
+package com.yi.cloud.common.constants;
+
+public interface CloudConstants {
+    /**
+     * 鉴权请求头名称
+     */
+    String AUTH_HEADER = "Authorization";
+
+
+    String TOKEN = "token";
+}
--- src/main/java/com/yi/cloud/common/exception/ApiServiceException.java
+++ src/main/java/com/yi/cloud/common/exception/ApiServiceException.java
@@ -3,7 +3,42 @@ package com.yi.cloud.common.exception;
 /**
  * 远程调用服务异常
  */
-public class ApiServiceException extends RuntimeException{
+public abstract class ApiServiceException extends Exception {
 
+    /**
+     * 错误编码
+     */
+    private Integer code;
 
+    /**
+     * 错误的提示信息
+     */
+    private String errorMessage;
+
+    public ApiServiceException(IBaseExceptionEnum exception) {
+        super(exception.getMessage());
+        this.code = exception.getCode();
+        this.errorMessage = exception.getMessage();
+    }
+
+    /**
+     * 获取异常的类的具体名称
+     */
+    public abstract String getExceptionClassName();
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
 }
